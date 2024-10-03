fixed leg_absolute_position to work with mount rotation

hexapod/hexapod/leg.py
@@ -31,9 +31,15 @@ class Leg:
     
     def get_absolute_joint_position(self):
         self.joints_abs_pos = [[0.0]*3 for i in self.joints_pos]
-        self.joints_abs_pos[0] = self.joints_pos[0]
+        self.joints_abs_angle = [[0.0]*3 for i in self.joints_pos]
+        self.joints_abs_pos[0] = self.joints_pos[0].copy()
+        self.joints_abs_angle[0] = self.joints_angle[0].copy()
         for i in range(1,len(self.joints_pos)):
-            self.joints_abs_pos[i] = self.rotation.rotate_point(*self.joints_pos[i],*self.joints_angle[i-1])
+            self.joints_abs_angle[i] = self.joints_angle[i].copy()
+            self.joints_abs_angle[i][0] += self.joints_abs_angle[i-1][0]
+            self.joints_abs_angle[i][1] += self.joints_abs_angle[i-1][1]
+            self.joints_abs_angle[i][2] += self.joints_abs_angle[i-1][2]
+            self.joints_abs_pos[i] = self.rotation.rotate_point(*self.joints_pos[i],*self.joints_abs_angle[i-1])
             self.joints_abs_pos[i][0] += self.joints_abs_pos[i-1][0]
             self.joints_abs_pos[i][1] += self.joints_abs_pos[i-1][1]
             self.joints_abs_pos[i][2] += self.joints_abs_pos[i-1][2]
@@ -54,7 +60,7 @@ class Leg:
             return [t, ratio*t, proj_mov]
         
         diff_vec = [final_foot_pos[0] - start_foot_pos[0] , final_foot_pos[1] - start_foot_pos[1]]
-        
+
         cond = diff_vec[0] < 0
         t_start = min(0,diff_vec[0])
         t_end = max(0,diff_vec[0])
@@ -76,12 +82,11 @@ class Leg:
 #     [0.0, -1.0, 0.0],
 # ]
 # leg.joints_angle = [
+#     [0.0, 0.0, 1.57],
 #     [0.0, 0.0, 0.0],
-#     [0.0, 0.0, 0.0],
-#     [0.0, 0.0, 3.14/2],
-#     [0.0, -3.14/2, 0.0],
+#     [0.0, 0.0, 1.57],
+#     [0.0, -1.57, 0.0],
 # ]
 # leg.get_absolute_joint_position()
 # print(leg.joints_abs_pos)
-# print(leg.joints_abs_angle)
 # pos = leg.get_leg_movement(np.radians(30), 30)

hexapod/hexapod/my_node.py
@@ -66,7 +66,7 @@ class TransformPublisher(Node):
             angle += np.pi/3
             time.sleep(0.01)
         self.stand_body()
-        self.move_leg(2)
+        self.move_leg(0)
 
     def stand_body(self):
         for i in range(0,6):
@@ -98,14 +98,21 @@ class TransformPublisher(Node):
             )
             self.transform_manager_static.broadcast_transform()
  
-    def move_leg(self, leg_index):
+    def move_leg(self, leg_index: int):
         leg = self.legs[leg_index]
-        joint_index = 1
-        # angles = self.rotation.inverse_kinematics()
+        joint_index = 0
         phi = np.radians(30) # stride angle
-        leg.joints_angle[joint_index][1] += phi
-        leg.joints_rot[joint_index] = self.rotation.euler_to_quaternion(*leg.joints_angle[joint_index])
 
+        # get foot and rotate it about joint_index by phi
+        leg.get_absolute_joint_position()
+        rot_foot = self.rotation.rotate_about_point(*leg.joints_abs_pos[-1],*leg.joints_abs_pos[joint_index],0,0,phi)
+
+        # update mount angle by phi, get foot
+        leg.joints_angle[joint_index][2 if joint_index == 0 else 1] += phi # only updated mount in z axis, other joints y axis
+        leg.joints_rot[joint_index] = self.rotation.euler_to_quaternion(*leg.joints_angle[joint_index])
+        leg.get_absolute_joint_position()
+
+        # broadcast transform for rotation
         self.transform_manager_static.set_transform(
             'body' if joint_index == 0 else leg.joints[joint_index-1],
             leg.joints[joint_index],